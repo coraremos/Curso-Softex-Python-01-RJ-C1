3b6ae099c7b8682b6639f79db917f3a454fa4726
Caroline Costa <65172953+corarwho@users.noreply.github.com>
2025-08-20 10:18:49 -0300

    desafio padaria

---
+++ desafio_aula_20-08.py
@@ -0,0 +1,19 @@
+'''
+comércio: Padaria Softex
+1- O programa tem que rodar em loop infinito até ser parado
+2- Cliente deve pedir um tipo de pão (pão francês, pão doce, pão de forma, australiano)
+3- Cada pão terá uma quantidade de estoque
+4- Cada pão terá um preço
+5- Pedir a forma de pagamento (dinheiro ou cartão)
+6- Forma de entrega
+7- Pedir dados do cliente (se for entregar nome e endereço, se for buscar só o nome)
+8- valor do frete por bairro
+9- nome do atendente
+10- codigo de entrega
+'''
+
+pedido = input('Olá, somos a Padaria Softex. Você deseja iniciar um pedido? (s/n): ')
+if pedido == s:
+    print('Então vamos começar! Qual pão você deseja? Temos as oções: \nFrancês,\nPão Doce,\nPão de forma,\nAustraliano.' )
+else:
+    print('Agradecemos a sua visita! Ficamos a disposição.')
