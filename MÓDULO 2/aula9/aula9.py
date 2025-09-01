d28c28178bba4d4895d0e3dd14dffb5fd5cb2af8
Caroline Costa <65172953+corarwho@users.noreply.github.com>
2025-09-01 11:40:59 -0300

    anotações da aula

---
+++ MÓDULO 2/aula9/aula9.py
@@ -0,0 +1,26 @@
+#variaveis estruturadas guardam mais de um valor (diferente das variáveis aprendidas anteriormente: str int float bool)
+
+''' 
+LISTAS: 
+
+.append() (se indefinido, adiciona no final)
+.insert()
+.remove()
+.pop() (se indefinido, remove no final)
+
+.join() (junta os itens em uma variavel unica tipo str)
+ESSA FUNÇÃO ACEITA LISTA COM --SOMENTE-- STRINGS!!!!
+'''
+partes = ["olá", "mundo"]
+frase = " ".join(partes)
+print(frase)
+'''
+.sort() organiza em ordem decrescente, em ordem alfabetica ou numerica.
+ou .sort(reverse=True) organiza de forma decrescente
+
+PILHAS(LIFO) E FILAS(FIFO)
+pilha - empilhar no último lugar/em cima ex.:pilha de pratos
+fila - a primeira a chegar será a primeira a ser atendida, 
+o primeiro a entrar é sempre o primeiro a sair
+
+'''
