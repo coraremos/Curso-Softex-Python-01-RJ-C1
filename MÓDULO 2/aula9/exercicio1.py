afaded69923ed01aa8bed4e16ebf6560f9610793
Caroline Costa <65172953+corarwho@users.noreply.github.com>
2025-09-01 11:41:08 -0300

    resolucao do exercicio 1

---
+++ MÓDULO 2/aula9/exercicio1.py
@@ -0,0 +1,26 @@
+count = 0
+lista = [8,7,9,8,4,1,5,4,8,4,6,8]
+numero = 8
+for i in lista:
+    if i == numero:
+        count += 1
+print(count)
+
+#ENUNCIADO
+'''
+Nível 1: Fundamentos
+Neste nível, você vai praticar a manipulação básica de listas, loops e condições simples.
+
+Exercício 1: Contando Ocorrências
+
+Crie um programa que conte quantas vezes um número específico aparece em uma lista.
+● Entrada: Uma lista de números e um número para ser procurado.
+● Saída: Um número inteiro que representa a quantidade de vezes que o número
+procurado aparece na lista.
+
+Exemplo:
+numeros = [1, 5, 2, 8, 5, 3, 5]
+numero_procurado = 5
+Resultado Esperado:
+3
+'''
