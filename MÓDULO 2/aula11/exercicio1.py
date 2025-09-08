d58fdfc71ced955954b0d32a6368cdbd07736037
corarwho <65172953+corarwho@users.noreply.github.com>
2025-09-08 11:49:39 -0300

    desenvolvimento do exercicio em aula

---
+++ MÓDULO 2/aula11/exercicio1.py
@@ -0,0 +1,38 @@
+#exercicio nivel5:
+#21
+
+agenda_contatos = {}
+
+while True:
+
+    print('---Agenda de contatos---')
+    print('1. Adicionar contato')
+    print('2. Buscar contato')
+    print('3. Ver lista de contatos')
+    print('4. Sair')
+
+    try:
+        escolha = input('Digite a opção desejada: ')
+        escolha = int(escolha)
+    except ValueError:
+        print('Opção inválida, digite uma das opções oferecidas:')
+        continue
+
+    if escolha == 1:
+        agenda_contatos["contato"] = input('Qual contato deseja adicionar?').lower()
+
+#estudar e corrigir essa parte
+    elif escolha == 2:
+        nome = input('Digite o contato que deseja encontrar: ')
+        if nome in agenda_contatos.value():
+            print(f'Contato {nome} já existe na agenda!')
+        else:
+            print(f'Contato {nome} não existe')
+
+    elif escolha == 3:
+        print(agenda_contatos')
+              
+    elif escolha == 4:
+        print('Finalizando a consulta.')
+    else:
+        print('Opção inválida, digite uma das opções oferecidas:')
