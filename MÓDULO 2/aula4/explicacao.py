6bd2ac8409edd68ea763d08631b3abfea0d548c2
corarwho <65172953+corarwho@users.noreply.github.com>
2025-09-10 10:17:28 -0300

    update

--- MÓDULO 2/aula4/explicacao.py
+++ MÓDULO 2/aula4/explicacao.py
@@ -5,3 +5,5 @@ def exibir_pedido(item: str, *extras: str, **observacoes: str) -> None:
 
 
 exibir_pedido("Pizza", "Queijo extra", "Bacon", borda="recheada")
+
+# -> None significa que ele vai me retornar nada, pois ele irá printar no final.
