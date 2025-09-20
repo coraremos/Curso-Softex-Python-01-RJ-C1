31a8d10e9bc7757a27997e907706ae0676096232
Caroline Costa <65172953+corarwho@users.noreply.github.com>
2025-09-20 11:48:13 -0300

    movendo entre pastas

--- MÓDULO 2/aula6/exercicios-casa/exercicio1.py
+++
@@ -1,3 +0,0 @@
-#criar uma classe, definir o __init__, criar atributos e instanciar objetos.
-
-#Crie uma classe chamada Pessoa. No "registro de nascimento" (__init__), toda pessoa deve ter um nome e uma idade.
