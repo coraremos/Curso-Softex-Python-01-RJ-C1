9db6aaecf52a4458b3cff93f8deac088fdb7594a
Caroline Costa <65172953+corarwho@users.noreply.github.com>
2025-09-20 18:38:47 -0300

    andamento do exercicio

---
+++ MÓDULO 2/aula6/exercicios-casa/exercicio11/biblioteca.py
@@ -0,0 +1,26 @@
+#Exercício 11: Biblioteca de Livros
+
+#Crie duas classes: Livro e Biblioteca.
+
+#1. A classe Livro terá atributos título e autor.
+class Livro:
+    def __init__(self, titulo:str, autor:str) -> None:
+        self.titulo = titulo
+        self.autor = autor
+
+#2. A classe Biblioteca terá um atributo acervo, que começa como uma lista vazia [].
+class Biblioteca:
+    def __init__(self, acervo:list):
+        self.acervo = acervo 
+
+    #adicionar_livro(livro)
+    def adicionar_livro(self, livro):
+        '''recebe um objeto Livro e o adiciona à lista acervo.'''
+        self.acervo.append(livro)    
+
+    def listar_livros(self):
+        '''percorre a lista acervo e imprime o título e o autor de cada livro.'''
+        for livro in self.acervo:
+            print(f"Título: {livro.titulo}, Autor: {livro.autor}")
+
+
