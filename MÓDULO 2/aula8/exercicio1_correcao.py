b9f4706258b9e80376facb851378a89c3b070149
corarwho <65172953+corarwho@users.noreply.github.com>
2025-09-24 11:29:35 -0300

    correcao do exercicio um dado em aula

---
+++ MÓDULO 2/aula8/exercicio1_correcao.py
@@ -0,0 +1,32 @@
+#1. Nível Fácil: Registro de Pessoas
+
+#Crie uma classe base Pessoa com um construtor que recebe nome e idade.
+class Pessoa:
+    def __init__(self, nome:str, idade:int):
+        self._nome = nome
+        self._idade = idade
+    
+    #Adicione um método apresentar() que imprime uma frase com o nome e a idade da pessoa.
+    def apresentar(self):
+        print(f"Olá, meu nome é {self._nome}, minha idade é {self._idade}")
+
+#Em seguida, crie uma classe Estudante que herda de Pessoa.
+class Estudante(Pessoa):
+    #O construtor de Estudante deve chamar o construtor da classe pai e adicionar um atributo para o curso.
+    def __init__(self, nome:str, idade:int, curso:str) -> None:
+        super().__init__(nome, idade)
+        self._curso = curso
+
+    #A classe Estudante deve sobrescrever o método apresentar() para incluir o curso na frase.
+    def apresentar(self):
+        print(f"Olá, meu nome é {self._nome}, minha idade é {self._idade} e meu curso é {self._curso}.")
+
+pessoa = Pessoa("Luzia", 64)
+estudante = Estudante("João", 33, "Gestão de Software")
+
+#Por fim, crie uma lista com um objeto Pessoa e um objeto Estudante. 
+lista_objetos:list[Pessoa] = [pessoa,estudante]
+#Itere sobre a lista e chame o método apresentar() para cada item, demonstrando o polimorfismo.
+
+for objeto in lista_objetos:
+    objeto.apresentar()
