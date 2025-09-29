b1555c7ad56dd2124a902a9dce90ae12972831f8
corarwho <65172953+corarwho@users.noreply.github.com>
2025-09-29 10:44:49 -0300

    quarto exercicio feito em aula na aula 10

---
+++ MÓDULO 2/aula9/exercicio4_aula.py
@@ -0,0 +1,31 @@
+'''
+4. Dispositivos de um Computador (Fácil/Médio)
+● Classes: Teclado, Mouse, Monitor e Computador.
+● Classes Teclado, Mouse, Monitor:
+○ Método: __init__ (sem atributos).
+○ Método: ligar() que imprime uma mensagem indicando que o dispositivo está ligado
+(ex: "O teclado foi ativado.").
+● Classe Computador:
+○ Atributos (Composição): teclado, mouse e monitor, que devem ser instâncias das
+classes correspondentes.
+○ Método: __init__ que inicializa os três atributos.
+○ Método: ligar_computador() que chama o método ligar() de cada um dos seus
+dispositivos.
+'''
+
+class Teclado:
+    def __init__(self):
+        pass
+
+class Mouse:
+    def __init__(self):
+        pass
+
+class Monitor:
+    def __init__(self):
+        pass
+
+
+class Computador:
+    pass
+
