39d5533441853701c03a283723c5d6634d93367f
coraremos <65172953+coraremos@users.noreply.github.com>
2025-10-22 11:54:47 -0300

    andamento do exercicio

--- MÓDULO 3/aula6/exemplo da aula/user_service.py
+++ MÓDULO 3/aula6/exemplo da aula/user_service.py
@@ -1,21 +1,30 @@
 # user_service.py
 from user_model import UserModel
 from hasher import hash_senha, verificar_senha
+from database import DatabaseConnection
 
 
 class UserService:
 
     def __init__(self):
+        self.user_model = UserModel()
         """
         crie um atributo que receberá a UserModel como composição
         """
 
     def _safe_user_data(self, user) -> dict | None:
+        
+        if user is None:
+            return None
+        user_dict = dict(user)
+        user_dict.pop('senha_hash', None)
+        return user_dict
         """
         este é um método privado que recebe um usuarios do banco.
         verifique se o usuários existe e então retorne ele sem a sua senha
-        caso ele ão exista retorne None
+        caso ele não exista retorne None
         """
+
 
     def _is_authorized(
         self,
@@ -24,12 +33,22 @@ class UserService:
         target_user_id: int,
         action: str,
     ) -> bool:
+        
+        if current_user_profile == 'Diretoria':
+            return True
+        elif target_user_id is None:
+            return False
+        elif action == "edit_self":
+            return current_user_id == target_user_id
+        else:
+            False
         """
         Método que verifica o perfil do usuários, se for Diretoria retorne true
         Se não tiver target_user_id retorn false
         Se  action == "edit_self" retorne current_user_id == target_user_id
         No geral retorn false
         """
+
 
     def register_user(
         self,
@@ -38,6 +57,15 @@ class UserService:
         nome_completo: str,
         perfil: str = "Afiliado",
     ) -> tuple[bool, str]:
+        
+        if len(senha) < 8:
+            return False
+        if len(email) <= 10 and '@' not in email and not email.endswith('.com'):
+            return False, "O email precisa ter o mínimo de 10 caracteres, ter '@' e terminar com '.com'."
+        if not nome_completo.isalpha():
+            return False, "nome só deve ter letras"
+        else:
+            senha = hash_senha(senha)
         """
         Método para criar um usuários.
         o campo senha deve ter no mínimo 8 caracteres, caso contrário retorne False a mensagem de erro.
@@ -47,6 +75,13 @@ class UserService:
         """
 
     def login_user(self, email: str, senha: str) -> tuple[dict | None, str]:
+
+        user = UserModel
+        if self.find_user_by_email(user) and senha == hash_senha:
+            return True
+
+
+
         """
         Este método é o login do usuários, deve receber um email e senha não vazios
         Use o método do find_user_by_email para buscar o usuario
