4b4655d62201f9d7d569f1bf7e0ea86000160eca
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-26 20:59:54 -0300

    resolvendo exercicios de lista

---
+++ MÓDULO 3/50exercicios_casa/#3.py
@@ -0,0 +1,8 @@
+'''Use um loop while para iterar sobre uma lista de strings. 
+Remova todas as strings que contêm a letra 'a' (minúscula ou maiúscula). 
+Atenção: Mudar o tamanho da lista durante a iteração requer cuidado 
+(ex: iterar de trás para frente ou usar um índice while).
+
+Ferramentas sugeridas: while, list.pop()/list.remove(), str.lower(), if, print'''
+
+
