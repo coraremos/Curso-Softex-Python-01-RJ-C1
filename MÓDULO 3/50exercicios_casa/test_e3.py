c7669451edc3d0a34801b805cf8156b40d7903b5
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-27 22:18:53 -0300

    movendo de pasta

--- MÓDULO 3/50exercicios_casa/test_e3.py
+++
@@ -1,6 +0,0 @@
-from e3 import editar_lista
-
-def test_editar_lista():
-    lista = ['a', 'b', 'A', 'c']
-    saida = ['b', 'c']
-    assert editar_lista(lista) == saida
--- MÓDULO 3/50exercicios_casa/test_e4.py
+++
@@ -1,6 +0,0 @@
-from e4 import cria_lista_numeros
-
-def test_cria_lista_numeros():
-    lista_mesclada = ['a', False, 'b', 3, 2]
-    saida = [3, 2]
-    assert cria_lista_numeros(lista_mesclada) == saida
