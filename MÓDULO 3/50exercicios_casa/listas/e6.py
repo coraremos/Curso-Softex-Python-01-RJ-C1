b93f8e87dbca65c3824a7553cb4051a052348a69
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-27 22:19:22 -0300

    exercicio em andamento

---
+++ MÓDULO 3/50exercicios_casa/listas/e6.py
@@ -0,0 +1,10 @@
+"""
+Dada uma lista de listas (matriz $N \times M$), 
+crie uma função para transpor a matriz (linhas viram colunas). 
+Use list comprehension aninhada.
+
+função, list comprehension (aninhada), range(), print
+
+https://www.youtube.com/watch?v=M2zL6LnQwkw
+"""
+
