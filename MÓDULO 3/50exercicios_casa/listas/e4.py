e412735e0cc4491b83a6cc5238081f65b8ff0b35
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-27 22:19:13 -0300

    movendo de pasta

---
+++ MÓDULO 3/50exercicios_casa/listas/e4.py
@@ -0,0 +1,16 @@
+'''
+Dada uma lista, use list comprehension para criar 
+uma nova lista que retém apenas os números, 
+descartando strings, booleanos ou outro
+
+Usar list comprehension, isinstance(), if, print
+'''
+
+def cria_lista_numeros(lista:list):
+    lista_numeros = [item for item in lista if type(item) is int]
+
+    return lista_numeros
+
+lista_mesclada = ['a', False, 'b', 3, 2]
+print(cria_lista_numeros(lista_mesclada))
+
---
+++ MÓDULO 3/50exercicios_casa/listas/test_e5.py
@@ -0,0 +1,6 @@
+from e5 import verificar_duplicatas
+
+def test_verificar_duplicadas():
+    lista = ['a', 'b', 'c', 'd', 'a', 'f', 'b', 'c', 'a']
+    saida = [('a', 3), ('b', 2), ('c', 2)]
+    assert verificar_duplicatas(lista) == saida
