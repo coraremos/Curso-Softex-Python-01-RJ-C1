273d5a65bd6a7ea533cafd5a85d45d4e59a3c71e
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-28 16:19:36 -0300

    del

--- MÓDULO 3/50exercicios_casa/listas/test_e5.py
+++
@@ -1,7 +0,0 @@
-from e5 import verificar_duplicatas
-import pytest
-
-def test_verificar_duplicadas():
-    lista = ['a', 'b', 'c', 'd', 'a', 'f', 'b', 'c', 'a']
-    saida = [('a', 3), ('b', 2), ('c', 2)]
-    assert verificar_duplicatas(lista) == saida
