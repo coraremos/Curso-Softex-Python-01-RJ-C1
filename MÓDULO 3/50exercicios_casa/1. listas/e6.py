1d1ae88bb6dbd5a333b7f6834f6a7d1bb89125d2
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-28 17:26:59 -0300

    atualizando o exercicio

--- MÓDULO 3/50exercicios_casa/1. listas/e6.py
+++ MÓDULO 3/50exercicios_casa/1. listas/e6.py
@@ -15,26 +15,10 @@ def transpor_matriz(matriz: list[list]) -> list[list]:
     if not matriz:
         return []
     col_count = len(matriz[0])
+    
     if any(len(linha) != col_count for linha in matriz):
         raise ValueError("A matriz deve ser retangular (todas as linhas com mesmo comprimento).")
     
     #list comprehension
     return [[linha[i] for linha in matriz] for i in range(col_count)]
-
-# retangular
-m = [[1,2,3],[4,5,6]]
-print(transpor_matriz(m))
-# == [[1,4],[2,5],[3,6]]
-
-# quadrado
-m = [[1,2],[3,4]]
-print(transpor_matriz(m)) 
-# == [[1,3],[2,4]]
-
-# vazio
-print(transpor_matriz([])) 
-# == []
-
-# com pytest.raises(ValueError):
-print(transpor_matriz([[1,2],[3]]))
 
