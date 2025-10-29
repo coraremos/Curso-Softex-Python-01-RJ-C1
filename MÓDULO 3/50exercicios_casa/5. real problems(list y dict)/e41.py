7d3ee2d8af2043ba7dac96bd886c8a109aa0b876
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-28 22:36:44 -0300

    exercicio 41 em andamento

---
+++ MÓDULO 3/50exercicios_casa/5. real problems(list y dict)/e41.py
@@ -0,0 +1,9 @@
+'''Simulador de Pilha (LIFO)
+
+Crie uma classe Pilha. 
+Use uma lista como estrutura de dados interna. 
+Implemente os métodos push(), pop() (com try/except para lista vazia) e is_empty().
+
+class, list, função (método), try/except (IndexError)
+'''
+
