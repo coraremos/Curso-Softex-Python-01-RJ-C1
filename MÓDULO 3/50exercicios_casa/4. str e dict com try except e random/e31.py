b1b3c8b51b3050c99b54bb7fe51ae86f7174db59
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-28 22:36:37 -0300

    exercicio 31 em andamento

---
+++ MÓDULO 3/50exercicios_casa/4. str e dict com try except e random/e31.py
@@ -0,0 +1,9 @@
+'''Geração de Matrícula Aleatória
+
+Crie uma função que gere um ID de matrícula de 8 caracteres, 
+sendo os 4 primeiros letras maiúsculas aleatórias e os 4 últimos dígitos aleatórios. 
+Use random e métodos de string (como str.join()).
+
+função, random.choice(), str.join(), print
+'''
+
