fc963c0191d967593ef44fe447a5f009bc10d000
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-10-28 22:36:31 -0300

    exercicio 21 em andamento

---
+++ MÓDULO 3/50exercicios_casa/3. list e dict (POO e Integr)/e21.py
@@ -0,0 +1,23 @@
+'''Classe Base ItemInventario
+
+Crie uma classe ItemInventario com atributos: 
+nome (string), quantidade (int) e valor_unitario (float). 
+Implemente um método valor_total().
+
+class, __init__, função (método)
+'''
+
+class TemInventario:
+    def __init__(self, nome:str, quantidade:int, valor_unitario:float):
+        self.nome = nome
+        self.quantidade = quantidade
+        self.valor_unitario = valor_unitario
+
+    def produtos(dict):
+        
+
+    def valor_total(quantidade):
+        if quantidade > 0:
+            return "Há {quantidade} itens desse produto no inventário."
+        return 'Não há itens no inventário'
+
