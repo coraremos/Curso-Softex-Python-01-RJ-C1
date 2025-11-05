f0503aad5c12ff404c7558108fd505d007891692
coraremos <65172953+coraremos@users.noreply.github.com>
2025-11-05 11:39:45 -0300

    observacoes da prova

---
+++ notas.py
@@ -0,0 +1,10 @@
+# ~ ativar o ambiente virtual
+## py -m venv .venv
+## .venv\Scripts\activate
+## pip install -r .\requirements.txt
+## pip list
+
+# ~ ativar o projeto
+## cd modulo_4
+## python manage.py runserver
+## http://127.0.0.1:8000
