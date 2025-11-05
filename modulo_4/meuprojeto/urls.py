caaab8005178298ae208a5b2a27aeaa7f9e206a9
coraremos <65172953+coraremos@users.noreply.github.com>
2025-11-05 11:40:01 -0300

    aula de hj

---
+++ modulo_4/meuprojeto/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for meuprojeto project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    #Qualquer URL que chegar no site (representada pelo '') deve ser redirecionada para o arquivo core/urls.py para ser processada.
+    path('',include('core.urls')),
+]
