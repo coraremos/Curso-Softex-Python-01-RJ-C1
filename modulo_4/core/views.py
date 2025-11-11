49f928bb20b9b07101f68a80fb7b17130db9c482
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-11-11 15:49:13 -0300

    atualizacao do views

--- modulo_4/core/views.py
+++ modulo_4/core/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-
+from .models import Tarefa  # 1. Importe o Model Tarefa
 # Create your views here.
 from django.http import HttpResponse
 
 #uma view é uma função que recebe um 'request' e retorna uma 'response'
 def home(request):
+   
+    # 2. Use o ORM para buscar os dados!     
+    # Tarefa.objects.all() significa: "Pegue todas as linhas da tabela Tarefa"    
+    todas_as_tarefas = Tarefa.objects.all()
+    
     #vamos retornar a resposta HTTP mais simples: um texto HTML
     # return HttpResponse("<h1>Olá, Mundo! Esta é a primeira página Django da Corar!</h1>")
     context = {
         'nome_usuario':'Júnior',
-        'tecnologias':['Python','Django','HTML','CSS']
+        'tecnologias':['Python','Django','HTML','CSS'],
+        'tarefas': todas_as_tarefas  # 4. Adicione as tarefas ao contexto
     }
+
+
+
     return render(request,'home.html',context)
 
