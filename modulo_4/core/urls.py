a4c7a3121e7ac64569f056ea8a11762c38255486
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-11-16 21:35:58 -0300

    revisao para aula

--- modulo_4/core/urls.py
+++ modulo_4/core/urls.py
@@ -3,6 +3,14 @@ from . import views #O ponto importa as views do app atual
 
 urlpatterns = [
     #Quando a URL for a raiz (''), chame a função 'home' de views.py
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+
+    # <int:pk> captura um inteiro da URL e o passa para a view como um argumento chamado 'pk'
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
 ]
 
+
+#   <int:pk>: Esta é a sintaxe do Django para capturar partes de uma URL.  
+#             int significa queele espera um número inteiro, 
+#             e pk é o nome da variável que será passada para a nossa view. 
