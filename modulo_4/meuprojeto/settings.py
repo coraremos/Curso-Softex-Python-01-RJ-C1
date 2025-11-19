8b565898ad075da039f86651c17df3e78644b60f
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-11-18 22:56:34 -0300

    aplicando alteracoes para a prox aula

--- modulo_4/meuprojeto/settings.py
+++ modulo_4/meuprojeto/settings.py
@@ -122,3 +122,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static'] #QUANDO É CRIADA UMA VARIAVEL permanen
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login' 
