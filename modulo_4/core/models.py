fbe9465998b7bea2e4b4e0f035b8d38d9685d5af
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-11-18 22:56:21 -0300

    aplicando alteracoes para a prox aula

--- modulo_4/core/models.py
+++ modulo_4/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Cada classe aqui é "traduzida" para uma tabela no banco de dados 
 class Tarefa(models.Model):     
@@ -12,9 +13,14 @@ class Tarefa(models.Model):
     # auto_now_add=True: Salva a data e hora EXATAMENTE quando o objeto é criado     
     criada_em = models.DateTimeField(auto_now_add=True)      
     
+    # 'on_delete=models.CASCADE' diz ao banco: 
+    # "Se o usuário for deletado, delete todas as tarefas dele também."
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # Este é um "método mágico" do Python.     
     # Ele diz ao Django como "chamar" um objeto Tarefa.     
     # Em vez de "Tarefa object (1)", ele mostrará o título da tarefa.     
-    # Isso é EXTREMAMENTE útil no painel de administração.     
+    # Isso é EXTREMAMENTE útil no painel de administração. 
     def __str__(self):         
         return self.titulo
+
